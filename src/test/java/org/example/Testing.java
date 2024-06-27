6ca693c87f6a85039677f85d1b33119edee7d47a
yu-eltech <eltech4u@gmail.com>
2024-06-26 21:26:47 -0400

    my third push

--- src/test/java/org/example/Testing.java
+++ src/test/java/org/example/Testing.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Testing {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("OOPS");
+        System.out.println("Change");
     }
 }
