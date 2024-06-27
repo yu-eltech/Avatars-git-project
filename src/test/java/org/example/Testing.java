e740a60f22b7c5e9a2855064d536c183d9dd79d2
yu-eltech <eltech4u@gmail.com>
2024-06-27 10:12:08 -0400

    The latest change

--- src/test/java/org/example/Testing.java
+++ src/test/java/org/example/Testing.java
@@ -4,5 +4,6 @@ public class Testing {
     public static void main(String[] args) {
         System.out.println("OOPS");
         System.out.println("Change");
+        System.out.println("Change2");
     }
 }
